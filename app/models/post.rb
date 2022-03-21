9e2ec3106003c323b69cdb086e4516ba2c12b741
Andrei Manaila <andrei.manaila@takeofflabs.com>
2022-03-21 23:12:35 +0200

    capitalize post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,14 @@ class Post < ApplicationRecord
   has_many :favorite_users, through: :user_favorite_cars, source: :user
   has_many_attached :images  
   default_scope -> { order(created_at: :desc) }
+  before_save :capitalize_title
   validates :user_id, presence: true
   validates :title, presence: true, length: { minimum: 3, maximum: 25 }
   validates :content, length: { maximum: 140 }
   validates :images, content_type: { in: ['image/jpg', 'image/jpeg', 'image/png'], message: "must be a valid image format" }, 
                      size: { less_than: 5.megabytes, message: "should be less than 5MB" }
+
+  def capitalize_title
+    self.title = title.capitalize
+  end
 end
