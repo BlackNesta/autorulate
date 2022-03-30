51e4018e6d7d7e8416fb2686f1658c3641980cda
Andrei Manaila <andrei.manaila@takeofflabs.com>
2022-03-30 14:05:23 +0300

    update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user, only: :destroy
+  before_action :load_post, only: [:show, :edit, :update]
   
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -37,6 +37,9 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post.update(post_params)
+    @post.images.attach(params[:post][:images].values) if params[:post][:images].present?
+    redirect_to @post
   end
 
   def add_to_favorites
@@ -64,6 +67,10 @@ class PostsController < ApplicationController
     def correct_user
       @post = current_user.posts.find_by(id: params[:id])
       redirect_to root_url if @post.nil?
+    end
+
+    def load_post
+      @post = Post.find(params[:id])
     end
 
 end
