66086ccb2cf345ad3a1f269de8d382f2361dbd4f
Andrei Manaila <andrei.manaila@takeofflabs.com>
2022-04-20 22:18:01 +0300

    add slideshow

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,6 @@
 module PostsHelper
-  def display_image(image)
-    image.variant(resize_to_limit: [400, 400])
+  def display_image(image, size = 400)
+    image.variant(resize_to_limit: [size, size])
   end
+
 end
